71a9f1cf806de79fe979de5a270c3e1d84430574
support-ing <vas0503march@gmail.com>
2019-11-01 13:25:56 +0100

    Правка новостей

--- views/post/show.php
+++ views/post/show.php
@@ -10,12 +10,14 @@ $img = $model->getBanner();
 $this->params['breadcrumbs'][] = ['label' => 'Новости', 'url' => '/post'];
 $this->params['breadcrumbs'][] = ['label' => $model->title,];
 ?>
+
 <div class="container adm">
 	 <a id="ios-back" onclick="javascript:history.back(); return false;" сlass=""> <img сlass="ios-back" src="/images/home/icons8-long-arrow-left-50.png" alt=""></a>
 <div class="news-banner">
-<?= Html::img($img, ['alt' => $model->title, 'width'=>'100%'])?>
-<h1 class="news-header"><?=$model->title ?></h1>
+	<?= Html::img($img, ['alt' => $model->title, 'width'=>'100%'])?>
+	<h1 class="news-header"><?=$model->title ?></h1>
 </div>
-<p><?=$model->description ?></p>
+
+	<p><?=$model->description ?></p>
 
 </div>
